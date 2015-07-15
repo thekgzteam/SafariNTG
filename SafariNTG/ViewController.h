f15c97fce30c8a9e5f6f4b82b3e91edf1b53b57f
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-07-15 08:18:59 -0700

    Initial Commit

---
+++ SafariNTG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafariNTG
+//
+//  Created by Edil Ashimov on 7/15/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
